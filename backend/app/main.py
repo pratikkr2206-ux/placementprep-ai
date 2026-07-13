dc81a5d92df044a97252b9b3ba39a1ff6b39e1e8
Pratik Kumar <prtkur44@gmail.com>
2026-07-13 21:34:14 +0530

    Enable SSL for Aiven MySQL

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,8 +36,8 @@ app.add_exception_handler(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        #"http://localhost:3000",
-        settings.FRONTEND_URL,
+        "http://localhost:3000",
+        "https://placementprep-ai-gamma.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
